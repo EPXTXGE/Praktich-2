61a1d2c3971fa672b273e9631225fe3eaf17bb96
КУПК ИТ <kupc_visual@outlook.com>
2020-09-28 18:06:48 +0500

    vetv1

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp14
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("парапр");
+            Console.ReadLine();
         }
     }
 }
